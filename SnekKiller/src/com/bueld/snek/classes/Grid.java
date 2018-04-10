5dc5b3c388bbb83b275b69c01a989c0810354ba5
Wiegel.Bela <Wiegel.Bela@SK-901B0E96984B.2817.skool.net>
2018-04-10 09:46:30 +0200

    sligthtly differences

--- SnekKiller/src/com/bueld/snek/classes/Grid.java
+++ SnekKiller/src/com/bueld/snek/classes/Grid.java
@@ -1,32 +1,33 @@
 package com.bueld.snek.classes;
 
+import javafx.scene.Group;
 import javafx.scene.shape.Rectangle;
 
-public class Grid {
-	
-	private Rectangle[][] grid;
+public class Grid extends Group {
 
 	public Grid(int size, int playerID, Rectangle style) {
-		grid = new Rectangle[size][size];
-		
+
 		create(size, style);
 	}
-	
+
 	public void create(int size, Rectangle style) {
-		
+
 		// i is Letters
 		// j is Numbers
-		
-		for (int i = 0;i<size;i++) {
-			for (int j = 0;j<size;j++) {
-				grid[i][j] = style;
-				grid[i][j].setTranslateX(i*style.getWidth());
-				grid[i][j].setTranslateY(j*style.getHeight());
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				Rectangle r = style;
+				r.setTranslateX(i * style.getWidth());
+				r.setTranslateY(j * style.getHeight());
+
+				this.getChildren().add(r);
 			}
 		}
+
 	}
-	
+
 	public void addObjects() {
-		
+
 	}
 }
